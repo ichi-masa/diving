af40a72ec8ba87d66913ffaffe1cf6f663cd3f4d
ichimasa <smac.girl@gmail.com>
2023-09-19 16:07:58 +0900

    0919

--- src/js/script.js
+++ src/js/script.js
@@ -49,4 +49,67 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
             },
         }
     });
+
+// 背景色後の画像アニメーション //
+    //要素の取得とスピードの設定
+    const box = $('.color-animation'),
+    speed = 700;  
+
+    //.colorboxの付いた全ての要素に対して下記の処理を行う
+    box.each(function(){
+    $(this).append('<div class="color-animation__bg"></div>')
+    const color = $(this).find($('.color-animation__bg')),
+    image = $(this).find('img');
+    const counter = 0;
+
+    image.css('opacity','0');
+    color.css('width','0%');
+    //inviewを使って背景色が画面に現れたら処理をする
+    color.on('inview', function(){
+        if(counter == 0){
+            $(this).delay(200).animate({'width':'100%'},speed,function(){
+                image.css('opacity','1');
+                $(this).css({'left':'0' , 'right':'auto'});
+                $(this).animate({'width':'0%'},speed);
+                })
+            counter = 1;
+        }
+    });
+});
+
+
+// ページトップ //
+    $(function() {
+        const pageTop = $('.js-pagetop');
+        pageTop.hide();
+        //スクロールしたら
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 100) {  //100pxスクロールしたら表示
+                pageTop.fadeIn();
+            } else {
+                pageTop.fadeOut();
+            }
+        });
+        pageTop.on("click",function () {
+            $('body,html').animate({ //animate関数を参照
+                scrollTop: 0
+            }, 500);
+        });
+        return false;
+    });
+
+    // //フッター手前で止まるボタン
+    // $(document).ready(function () {
+    // $(".js-pagetop").hide();
+    // $(window).on("scroll", function () {
+    //     scrollHeight = $(document).height(); //ドキュメントの高さ
+    //     scrollPosition = $(window).height() + $(window).scrollTop(); //現在の位置
+    //     footHeight = $("footer").innerHeight(); //フッターの高さ
+    //     if (scrollHeight - scrollPosition <= footHeight) { //ドキュメントの高さと現在の位置の差がフッターの高さ以下のとき
+    //         $(".js-pagetop").css({ position: "absolute", bottom: footHeight + 20 }); //positionをabsoluteに変更
+    //     } else { //それ以外の場合は
+    //         $(".js-pagetop").css({ position: "fixed", bottom: "rem(20)" }); //固定で表示
+    //     }
+    // });
+    // });
 });
