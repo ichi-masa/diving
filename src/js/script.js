efa58b8ab411399fbacc8b7cc1d242e6e1f1d86a
ichimasa <smac.girl@gmail.com>
2023-09-20 15:38:29 +0900

    0920

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
+
 // ハンバーガーメニュー //
     $(function(){
         $('.hamburger').on('click', function() {
@@ -30,7 +31,7 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
 // campaign スライダー（Swiper）//
     const campaignSwiper = new Swiper(".campaign__Swiper", {
         loop: true,
-        slidesPerView: 1.2,
+        slidesPerView: 1.26,
         spaceBetween: 24,
         navigation: {
             nextEl: ".swiper-button-next",
@@ -44,38 +45,37 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
 
         breakpoints: {
             768: {
-                slidesPerView: 3.4,
+                slidesPerView: 3.48,
                 spaceBetween: 40,
             },
         }
     });
 
-// 背景色後の画像アニメーション //
     //要素の取得とスピードの設定
-    const box = $('.color-animation'),
+    const box = $('.js-colorbox'),
     speed = 700;  
 
-    //.colorboxの付いた全ての要素に対して下記の処理を行う
+    //.js-colorboxの付いた全ての要素に対して下記の処理を行う
     box.each(function(){
-    $(this).append('<div class="color-animation__bg"></div>')
-    const color = $(this).find($('.color-animation__bg')),
-    image = $(this).find('img');
-    const counter = 0;
+        $(this).append('<div class="color"></div>')
+        const color = $(this).find($('.color'));
+        const image = $(this).find('img');
+        let counter = 0;
 
-    image.css('opacity','0');
-    color.css('width','0%');
-    //inviewを使って背景色が画面に現れたら処理をする
-    color.on('inview', function(){
-        if(counter == 0){
-            $(this).delay(200).animate({'width':'100%'},speed,function(){
+        image.css('opacity','0');
+        color.css('width','0%');
+        //inviewを使って背景色が画面に現れたら処理をする
+        color.on('inview', function(){
+            if(counter == 0){
+                $(this).delay(200).animate({'width':'100%'},speed,function(){
                 image.css('opacity','1');
                 $(this).css({'left':'0' , 'right':'auto'});
                 $(this).animate({'width':'0%'},speed);
                 })
-            counter = 1;
-        }
+                counter = 1;
+            }
+        });
     });
-});
 
 
 // ページトップ //
@@ -98,18 +98,23 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
         return false;
     });
 
-    // //フッター手前で止まるボタン
-    // $(document).ready(function () {
-    // $(".js-pagetop").hide();
-    // $(window).on("scroll", function () {
-    //     scrollHeight = $(document).height(); //ドキュメントの高さ
-    //     scrollPosition = $(window).height() + $(window).scrollTop(); //現在の位置
-    //     footHeight = $("footer").innerHeight(); //フッターの高さ
-    //     if (scrollHeight - scrollPosition <= footHeight) { //ドキュメントの高さと現在の位置の差がフッターの高さ以下のとき
-    //         $(".js-pagetop").css({ position: "absolute", bottom: footHeight + 20 }); //positionをabsoluteに変更
-    //     } else { //それ以外の場合は
-    //         $(".js-pagetop").css({ position: "fixed", bottom: "rem(20)" }); //固定で表示
-    //     }
-    // });
-    // });
+    //フッター手前で止まるボタン
+    $(".js-pagetop").hide();
+    $(window).on("scroll", function () {
+    const scrollHeight = $(document).height();
+    const scrollPosition = $(window).height() + $(window).scrollTop();
+    const footHeight = $("footer").innerHeight();
+    if (scrollHeight - scrollPosition <= footHeight) {
+    // ページトップボタンがフッター手前に来たらpositionとfixedからabsoluteに変更
+        $(".js-pagetop").css({
+        position: "absolute",
+        bottom: footHeight + 20,
+        });
+    } else {
+        $(".js-pagetop").css({
+        position: "fixed",
+        bottom: "20px",
+        });
+    }
+    });
 });
