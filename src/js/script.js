49fb0235664bc2c78bc56f9e659a81ca4397d1ef
ichimasa <smac.girl@gmail.com>
2023-09-28 13:40:22 +0900

    0928_ローディングなし

--- src/js/script.js
+++ src/js/script.js
@@ -1,55 +1,81 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
-// ローディングアニメーション
-    $(function() {
-        setTimeout(function(){
-            $('.js-loading-title').fadeIn(2000);
-        },500);
-        setTimeout(function(){
-            $('.js-loading-title').fadeOut(500);
-        },500);
+    // ローディングアニメーション
+
+    // $(function () {
+    //     let webStorage = function () {
+    //         if (sessionStorage.getItem('access')) {
+    //             //2回目以降アクセス時の処理
+    //             $(".loading").addClass('is-active');
+    //         } else {
+    //             //初回アクセス時の処理
+    //             sessionStorage.setItem('access', 'true'); // sessionStorageにデータを保存
+    //             setTimeout(function () {
+    //                 $('.loading__title').addClass('is-active');
+    //             }, 0);
+    //             setTimeout(function () {
+    //                 $('.loading__left').addClass('is-active');
+    //             }, 500);
+    //             setTimeout(function () {
+    //                 $('.loading__right').addClass('is-active');
+    //             }, 1000);
+    //             setTimeout(function () {
+    //                 // ローディングを数秒後に非表示にする
+    //                 $(".loading").addClass('is-active');
+    //             }, 3000); // ローディングを表示する時間
+    //         }
+    //     }
+    //     webStorage();
+    // });
+
+    
+    // $(window).on('load', function(){
+    //     setTimeout(function () {
+    //         $('.loading__title').addClass('is-active');
+    //     }, 0);
+    //     setTimeout(function () {
+    //         $('.loading__left').addClass('is-active');
+    //     }, 500);
+    //     setTimeout(function () {
+    //         $('.loading__right').addClass('is-active');
+    //     }, 600);
+    //     setTimeout(function () {
+    //         $('.loading__title--white').addClass('is-active');
+    //     }, 1000);
+    //     setTimeout(function () {
+    //         // ローディングを数秒後に非表示にする
+    //         $(".loading").addClass('is-active');
+    //     }, 3000); // ローディングを表示する時間
+
+    // });
+
+    // ハンバーガーメニュー //
+    $(function () {
+        $('.hamburger').on('click', function () {
+            $(this).toggleClass('active');
+            $('.header').toggleClass('active');
+            return false;
+        });
     });
 
-    $(window).on('load', function() {
-        $('.loading__left').addClass('slide');  // 'slide' クラスを追加
-        $('.loading__right').addClass('slide');  // 'slide' クラスを追加
-        
-        setTimeout(function() {
-            $('.js-loading-title-white').fadeIn(2000);
-        }, 6000);
-
-        setTimeout(function() {
-            $('.js-loading').fadeOut(3000);
-        }, 6500);
+    // ドロワーメニュー //
+    $(function () {
+        $('.hamburger').on('click', function () {
+            $('.sp-nav').toggleClass('active');
+            return false;
+        });
     });
 
-// ハンバーガーメニュー //
-    $(function(){
-        $('.hamburger').on('click', function() {
-        $(this).toggleClass('active');
-        $('.header').toggleClass('active');
-        return false;
-    });
-    });
-
-// ドロワーメニュー //
-    $(function(){
-        $('.hamburger').on('click', function() {
-        $('.sp-nav').toggleClass('active');
-        return false;
-    });
+    // ドロワーメニュー下の背景固定 //
+    $(function () {
+        $('.hamburger').on('click', function () {
+            $('body').toggleClass('active');
+            return false;
+        });
     });
 
- // ドロワーメニュー下の背景固定 //
-    $(function(){
-        $('.hamburger').on('click', function() {
-        $('body').toggleClass('active');
-        return false;
-    });
-    });   
-
-// mainview スライダー（Swiper）//
+    // mainview スライダー（Swiper）//
     const mvSwiper = new Swiper(".mainview__Swiper", {
         loop: true,
         effect: 'fade',
@@ -60,7 +86,7 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
         speed: 3000,
     });
 
-// campaign スライダー（Swiper）//
+    // campaign スライダー（Swiper）//
     const campaignSwiper = new Swiper(".campaign__Swiper", {
         loop: true,
         slidesPerView: "1.26",
@@ -89,24 +115,24 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
 
     //要素の取得とスピードの設定
     const box = $('.js-colorbox'),
-    speed = 700;  
+        speed = 700;
 
     //.js-colorboxの付いた全ての要素に対して下記の処理を行う
-    box.each(function(){
+    box.each(function () {
         $(this).append('<div class="color"></div>')
         const color = $(this).find($('.color'));
         const image = $(this).find('img');
         let counter = 0;
 
-        image.css('opacity','0');
-        color.css('width','0%');
+        image.css('opacity', '0');
+        color.css('width', '0%');
         //inviewを使って背景色が画面に現れたら処理をする
-        color.on('inview', function(){
-            if(counter == 0){
-                $(this).delay(200).animate({'width':'100%'},speed,function(){
-                image.css('opacity','1');
-                $(this).css({'left':'0' , 'right':'auto'});
-                $(this).animate({'width':'0%'},speed);
+        color.on('inview', function () {
+            if (counter == 0) {
+                $(this).delay(200).animate({ 'width': '100%' }, speed, function () {
+                    image.css('opacity', '1');
+                    $(this).css({ 'left': '0', 'right': 'auto' });
+                    $(this).animate({ 'width': '0%' }, speed);
                 })
                 counter = 1;
             }
@@ -114,8 +140,8 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     });
 
 
-// ページトップ //
-    $(function() {
+    // ページトップ //
+    $(function () {
         const pageTop = $('.js-pagetop');
         pageTop.hide();
         //スクロールしたら
@@ -126,7 +152,7 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
                 pageTop.fadeOut();
             }
         });
-        pageTop.on("click",function () {
+        pageTop.on("click", function () {
             $('body,html').animate({ //animate関数を参照
                 scrollTop: 0
             }, 500);
@@ -137,20 +163,20 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     //フッター手前で止まるボタン
     $(".js-pagetop").hide();
     $(window).on("scroll", function () {
-    const scrollHeight = $(document).height();
-    const scrollPosition = $(window).height() + $(window).scrollTop();
-    const footHeight = $("footer").innerHeight();
-    if (scrollHeight - scrollPosition <= footHeight) {
-    // ページトップボタンがフッター手前に来たらpositionとfixedからabsoluteに変更
-        $(".js-pagetop").css({
-        position: "absolute",
-        bottom: footHeight + 20,
-        });
-    } else {
-        $(".js-pagetop").css({
-        position: "fixed",
-        bottom: "20px",
-        });
-    }
+        const scrollHeight = $(document).height();
+        const scrollPosition = $(window).height() + $(window).scrollTop();
+        const footHeight = $("footer").innerHeight();
+        if (scrollHeight - scrollPosition <= footHeight) {
+            // ページトップボタンがフッター手前に来たらpositionとfixedからabsoluteに変更
+            $(".js-pagetop").css({
+                position: "absolute",
+                bottom: footHeight + 20,
+            });
+        } else {
+            $(".js-pagetop").css({
+                position: "fixed",
+                bottom: "20px",
+            });
+        }
     });
 });
